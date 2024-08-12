1b0839f0e946fc9744a8eb7be706921f169564b8
Anuj Jain <52431257+AnujJain100@users.noreply.github.com>
2024-08-13 07:58:54 +0900

    Update BuildConfig2.java

--- samples/ar-model-viewer-java/src/main/java/com/google/ar/sceneform/samples/gltf/BuildConfig2.java
+++ samples/ar-model-viewer-java/src/main/java/com/google/ar/sceneform/samples/gltf/BuildConfig2.java
@@ -1,5 +1,5 @@
 package com.google.ar.sceneform.samples.gltf;
 
 public class BuildConfig2 {
-    public static String apiKey = "AIzaSyDZeuZ17I03G1H0dWUNopRdoBnBrcWgiOI";
+    public static String apiKey = "";
 }
